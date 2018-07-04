639f6313b49922e456b6635d7652fddc8f79f2a9
Elvio <elviobg@gmail.com>
2018-07-04 11:43:04 -0300

    Movendo a pasta core

--- todoTasks/urls.py
+++ todoTasks/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from core.views import home
+from todoTasks.core.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),    
