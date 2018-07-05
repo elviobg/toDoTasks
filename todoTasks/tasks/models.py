75364fd95573cc7d1b830d1409ed70c2e3e98882
Elvio <elviobg@gmail.com>
2018-07-05 11:31:15 -0300

    Adiciona o modelo das taarefas

--- todoTasks/tasks/models.py
+++ todoTasks/tasks/models.py
@@ -3,3 +3,16 @@ from django.db import models
 class Category(models.Model):
   name = models.CharField(max_length=64, verbose_name='Nome')
   description = models.TextField(verbose_name='Descrição')
+
+class Task(models.Model):
+  PRIORITY_CHOICES = (
+    ('A', 'Alta'),
+    ('M', 'Média'),
+    ('B', 'Baixa')
+  )
+    
+  name = models.CharField(max_length=64, verbose_name='Nome')
+  description = models.TextField(verbose_name='Descrição', blank=True)
+  final_date = models.DateField(verbose_name='Data final')
+  priority = models.CharField(max_length=1, verbose_name='Prioridade', choices=PRIORITY_CHOICES)
+  category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Categoria')
