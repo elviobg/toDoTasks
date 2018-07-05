d3bf2f63b854ff379a25836872bcccebf2423b4a
Elvio <elviobg@gmail.com>
2018-07-05 17:36:17 -0300

    Organiza tela de categorias para listar
    Muda a tela de inserção de nova categoria para lista de categorias com um botão no fim da página para inserir nova categoria

--- todoTasks/tasks/urls.py
+++ todoTasks/tasks/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from .views import new_category, new_task
+from .views import new_category, new_task, list_categories
 
 app_name = 'tasks'
 
 urlpatterns = [
+  #Create
   path('category/insert', new_category, name='url_category_insert'),
   path('task/insert', new_task, name='url_task_insert'),
+  #read
+  path('category', list_categories, name='url_category'),
+  #update
+  #delete
 ]
--- todoTasks/tasks/views.py
+++ todoTasks/tasks/views.py
@@ -1,13 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .forms import CategoryForm, TaskForm
+from .models import Category, Task
+
+def list_categories(request):
+  data = {}
+  data['categories'] = Category.objects.all()
+  return render(request, 'category/list.html', data)
 
 def new_category(request):
   if request.method == 'POST':
     form = CategoryForm(request.POST)
     if form.is_valid():
-      form.save()
-      return HttpResponse('Categoria adicionada com sucesso')
+      form.save()      
+      return redirect('tasks:url_category')
     else:
       print(form.errors)
   else:
