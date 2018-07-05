2e41d549fbf0d37017fc90fd84989761a96f975e
Elvio <elviobg@gmail.com>
2018-07-05 17:11:37 -0300

    Muda a tela de home para listar as tarefas

--- todoTasks/core/views.py
+++ todoTasks/core/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from todoTasks.tasks.models import Task
 
 # Create your views here.
 def home(request):
-  return render(request, 'base.html')
+  data = {}
+  data['tasks'] = Task.objects.all()
+  return render(request, 'index.html', data)
--- todoTasks/tasks/models.py
+++ todoTasks/tasks/models.py
@@ -3,6 +3,10 @@ from django.db import models
 class Category(models.Model):
   name = models.CharField(max_length=64, verbose_name='Nome')
   description = models.TextField(verbose_name='Descrição')
+  
+  class meta:
+    verbose_name_plural = 'Categorias'
+
   def __str__(self):
     return self.name
 
@@ -15,8 +19,12 @@ class Task(models.Model):
     
   name = models.CharField(max_length=64, verbose_name='Nome')
   description = models.TextField(verbose_name='Descrição', blank=True)
-  final_date = models.DateField(verbose_name='Data final')
+  final_date = models.DateTimeField(verbose_name='Data final')
   priority = models.CharField(max_length=1, verbose_name='Prioridade', choices=PRIORITY_CHOICES)
   category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Categoria')
+
+  class meta:
+    verbose_name_plural = 'Tarefas'
+
   def __str__(self):
     return self.name
