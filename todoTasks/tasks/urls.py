d63a03c1866c6ad6a2ca5502e5dca6b358bf1456
Elvio <elviobg@gmail.com>
2018-07-10 17:47:44 -0300

    Adiciona funcionalidade de editar e excluir categorias

--- todoTasks/tasks/urls.py
+++ todoTasks/tasks/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import create_category, create_task, list_categories, delete_task
+from .views import create_category, create_task, list_categories, update_task, delete_task, update_category, delete_category
 
 app_name = 'tasks'
 
@@ -9,8 +9,10 @@ urlpatterns = [
   path('task/insert', create_task, name='url_task_insert'),
   #read
   path('category', list_categories, name='url_category'),
-  #update
-  #path('category', update_task, name='url_category_update'),
+  #update  
+  path('task/update/<int:pk>', update_task, name='url_task_update'),
+  path('category/update/<int:pk>', update_category, name='url_category_update'),
   #delete
-  path('task/delete/<int:pk>', delete_task, name='url_category_delete'),  
+  path('task/delete/<int:pk>', delete_task, name='url_task_delete'),
+  path('category/delete/<int:pk>', delete_category, name='url_category_delete'),
 ]
--- todoTasks/tasks/views.py
+++ todoTasks/tasks/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404, get_list_or_404
 from django.http import HttpResponse
 from .forms import CategoryForm, TaskForm
 from .models import Category, Task
 
 def list_categories(request):
   data = {}
-  data['categories'] = Category.objects.all()
+  data['categories'] = get_list_or_404(Category)
   return render(request, 'category/list.html', data)
 
 def create_category(request):
@@ -36,10 +36,44 @@ def create_task(request):
   
   data = {}
   data['form'] = form
-  return render(request, 'task/new.html', data)  
+  return render(request, 'task/new.html', data)
 
 def delete_task(request, pk):
-  task = Task.objects.get(id=pk).delete()
+  get_object_or_404(Task, pk=pk).delete()
   return redirect('url_home')
 
-#def update_task(request, id):
+def update_task(request, pk):
+  task = get_object_or_404(Task, pk=pk)
+  if request.method == 'POST':    
+    form = TaskForm(request.POST, instance=task)
+    if form.is_valid():
+      form.save()
+      return redirect('url_home')
+    else:
+      print(form.errors)
+  else:
+    form = TaskForm(instance=task)
+
+  data = {}
+  data['form'] = form
+  return render(request, 'task/new.html', data)
+
+def delete_category(request, pk):
+  get_object_or_404(Category, pk=pk).delete()
+  return redirect('tasks:url_category')
+
+def update_category(request, pk):
+  category = get_object_or_404(Category, pk=pk)
+  if request.method == 'POST':    
+    form = CategoryForm(request.POST, instance=category)
+    if form.is_valid():
+      form.save()
+      return redirect('tasks:url_category')
+    else:
+      print(form.errors)
+  else:
+    form = CategoryForm(instance=category)
+
+  data = {}
+  data['form'] = form
+  return render(request, 'category/new.html', data)
