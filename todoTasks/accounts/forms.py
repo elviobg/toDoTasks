0ffc21b9c5ed31f69f04055abb081273b2afc119
Elvio <elviobg@gmail.com>
2018-07-11 21:58:57 -0300

    Estilização da tela de criação de usuario

--- todoTasks/accounts/forms.py
+++ todoTasks/accounts/forms.py
@@ -1,8 +1,13 @@
-from django.forms import ModelForm
+from django import forms
 from django.contrib.auth.models import User
 
-class UserForm(ModelForm):
+class UserForm(forms.ModelForm):  
+  password = forms.CharField(widget=forms.PasswordInput(), label="Senha")  
   class Meta:
-    model = User
-    #fields = (''__all__'')
+    model = User    
     fields = ('first_name', 'last_name', 'username', 'password')
+    labels = {
+      'first_name': 'Nome', 
+      'last_name': 'Sobrenome', 
+      'username': 'Login'
+      }
